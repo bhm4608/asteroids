9964c1055bc5020bf30feddd4bad317850cfe7e9
tr3lobite <mccarthybrendan700@gmail.com>
2025-10-26 23:10:42 -0700

    Initial commit with main file and constants file.

---
+++ main.py
@@ -0,0 +1,12 @@
+from constants import *
+import pygame
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()

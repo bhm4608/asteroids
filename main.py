0c8c352e7bd1cef4bca508c1021107eb8186d070
tr3lobite <mccarthybrendan700@gmail.com>
2025-10-26 23:45:40 -0700

    Added screen and game loop.

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # initialize screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+    
+
 
 if __name__ == "__main__":
     main()

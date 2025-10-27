a85b27d2bca7c4b100f0ff301b91388584ba4c14
tr3lobite <mccarthybrendan700@gmail.com>
2025-10-27 00:07:47 -0700

    Capped FPS to 60.

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from constants import *
 import pygame
 
 def main():
-    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     # initialize screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,6 +23,8 @@ def main():
             
         screen.fill(BLACK)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     
 
